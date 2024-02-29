0c87266e06f60dcfb0734668012ea76ea3701641
sallukha <sallukhan54159@gmail.com>
2024-02-29 06:15:15 -0800

    first commit

--- src/List1.js
+++ src/List1.js
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import './App.css'
-// function List1() {
-//   return (
-//     <div className='container18'>
-//          <h3 id='hoko'>Useful links</h3>
-//         <ul>
-//     <li>1</li>
-//     <li>1</li>
-//     <li>1</li>
-//     <li>1</li>
-//     <li>1</li>
+import React from 'react'
+import './App.css'
+function List1() {
+  return (
+    <div className='container18'>
+         <h3 id='hoko'>Useful links</h3>
+        <ul>
+    <li>1</li>
+    <li>1</li>
+    <li>1</li> 
+    <li>1</li>
+    <li>1</li>
   
 
-//         </ul>
-//     </div>
-//   )
-// }
+        </ul>
+    </div>
+  )
+}
 
-// export default List1
+export default List1
